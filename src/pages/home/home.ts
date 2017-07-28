d78f58ddd1b3d40f23581c43ca754c3466f9a4dc
Naim Jeem <naim36208@gmail.com>
2017-07-28 10:57:04 +0600

    app init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+import { MainPage } from '../main/main';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+  skipMsg: string = "Skip";
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  skip() {
+    this.navCtrl.push(MainPage);
+  }
+
+  slideChanged() {
+    if (this.slides.isEnd())
+      this.skipMsg = "Alright, I got it";
   }
 
 }
